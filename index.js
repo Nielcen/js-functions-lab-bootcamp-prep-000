c421eaf14bdab86519991bbb69385c0da2168a5e
James Ryan Nielcen <rnielcen@hotmail.com>
2018-01-14 17:34:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(you) {
+  return `Happy holidays, ${you}!`
+}
+function happyHolidayTo(IndependenceDay, you) {
+  return `Happy ${IndependenceDay}, ${you}!`
+}
+var x = 20
+function holidayCountdown(x, mothersDay) {
+
+}
